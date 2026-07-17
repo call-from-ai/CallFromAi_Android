730dda35ee6a821a9f408ebb4108c69bffa2375c
princehw03 <princehw03@gmail.com>
2026-07-18 07:40:47 +0900

    docs: LocalBottomBarPadding 사용 예시 및 가이드 추가
    - LocalBottomBarPadding.kt: Column 내에서 Spacer를 활용하여 하단 패딩을 적용하는 코드 예시 추가
    - LocalBottomBarPadding.kt: 플로팅 바텀바 디자인 요구사항(모서리 배경색 노출)에 따른 패딩 처리 필요성 설명 보강

--- core/designsystem/src/main/java/kr/co/call/designsystem/component/LocalBottomBarPadding.kt
+++ core/designsystem/src/main/java/kr/co/call/designsystem/component/LocalBottomBarPadding.kt
@@ -7,7 +7,7 @@ import androidx.compose.ui.unit.dp
 /**
  * 플로팅 바텀바의 실측 높이를 하위 컴포저블에 전달하는 CompositionLocal.
  *
- * 바텀바가 화면 위에 떠있는 구조(모서리 배경색 노출용)이기 때문에,
+ * 바텀바가 화면 위에 떠있는 구조(모서리 배경색 노출용)이기 때문에(요구사항.....하...),
  * 스크롤 가능한 컨텐츠는 이 값만큼 bottom padding을 주어야 바텀바에 가려지지 않는다.
  *
  * 사용 예시:
@@ -15,5 +15,18 @@ import androidx.compose.ui.unit.dp
  * val bottomBarPadding = LocalBottomBarPadding.current
  * LazyColumn(contentPadding = PaddingValues(bottom = bottomBarPadding))
  * ```
+ *
+ * ```kotlin
+ * val bottomBarPadding = LocalBottomBarPadding.current
+ * Column(){
+ *
+ *      //...//
+ *
+ *     Spacer(modifier = Modifier.height(bottomBarPadding))
+ * }
+ * ```
+ *
+ * 사용 예시 말고도 각자 화면에서 아래에 이 값을 사용해서 패딩을 줘야 할 듯 해유..아니 이게 진짜
+ * 요규사항이 바텀바가 화면 위에 떠있는 구조여야 바텀바 모서리 쪽을 만족시킬 수 있어요 ㅠㅠ
  */
 val LocalBottomBarPadding = compositionLocalOf<Dp> { 0.dp }
