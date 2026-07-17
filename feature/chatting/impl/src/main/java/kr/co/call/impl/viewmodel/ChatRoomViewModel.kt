71916b5116e8ef46470f7523159a17f7a4902d39
princehw03 <princehw03@gmail.com>
2026-07-17 22:06:56 +0900

    chore: 힐트뷰모델 어노테이션 추가

--- feature/chatting/impl/src/main/java/kr/co/call/impl/viewmodel/ChatRoomViewModel.kt
+++ feature/chatting/impl/src/main/java/kr/co/call/impl/viewmodel/ChatRoomViewModel.kt
@@ -1,12 +1,13 @@
 package kr.co.call.impl.viewmodel
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kr.co.call.domain.repository.ChatRepository
 import org.orbitmvi.orbit.Container
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.viewmodel.container
 import javax.inject.Inject
-
+@HiltViewModel
 class ChatRoomViewModel @Inject constructor(
     private val chatRepository: ChatRepository
 ): ViewModel(), ContainerHost<ChatRoomUiState, ChatRoomSideEffect> {
