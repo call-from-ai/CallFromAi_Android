c4a984862333c977097f374e84c7180d55ce1ea0
ljw42b <80021912+JiwonLee42@users.noreply.github.com>
2026-07-24 17:38:05 +0900

    refactor: #7 홈 도메인 모델 파일 통합

--- core/domain/src/main/java/kr/co/call/domain/model/home/HomeCharacter.kt
+++
@@ -1,9 +0,0 @@
-package kr.co.call.domain.model.home
-
-data class HomeCharacter(
-    val id: Long,
-    val name: String,
-    val relationshipDays: Int,
-    val imageUrl: String?,
-    val isMain: Boolean,
-)
