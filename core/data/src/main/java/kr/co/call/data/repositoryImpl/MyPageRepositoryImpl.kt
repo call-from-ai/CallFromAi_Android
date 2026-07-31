f96199063bf5435ee0ec353af1f5fbdd539e0281
WAcAW9 <wacaw9@gmail.com>
2026-07-31 14:02:03 +0900

    feat: 로그인/로그아웃 연결 (#51)

--- core/data/src/main/java/kr/co/call/data/repositoryImpl/MyPageRepositoryImpl.kt
+++ core/data/src/main/java/kr/co/call/data/repositoryImpl/MyPageRepositoryImpl.kt
@@ -1,12 +1,17 @@
 package kr.co.call.data.repositoryImpl
 
+import javax.inject.Inject
 import kotlinx.coroutines.delay
+import kr.co.call.data.push.PushTokenManager
+import kr.co.call.datastore.TokenDataStore
 import kr.co.call.domain.model.mypage.MyPageProfile
 import kr.co.call.domain.repository.MyPageRepository
-import javax.inject.Inject
 
-// TODO: API 연동 전 임시 구현체
-class MyPageRepositoryImpl @Inject constructor() : MyPageRepository {
+// TODO: 프로필 API 연동 전 임시 구현체 (logout/delete 는 토큰 정리 포함)
+class MyPageRepositoryImpl @Inject constructor(
+    private val pushTokenManager: PushTokenManager,
+    private val tokenDataStore: TokenDataStore,
+) : MyPageRepository {
 
     override suspend fun getMyProfile(): Result<MyPageProfile> {
         delay(500)
@@ -21,11 +26,19 @@ class MyPageRepositoryImpl @Inject constructor() : MyPageRepository {
         }
     }
 
+    /**
+     * 로그아웃: FCM 서버 해제 > 로컬 FCM 토큰 삭제 > JWT 삭제.
+     */
     override suspend fun logout() {
-        delay(300)
+        pushTokenManager.unregisterCurrentDevice()
+        tokenDataStore.clearTokens()
     }
 
+    /**
+     * 회원 탈퇴: 푸시 토큰/JWT 정리 (서버 탈퇴 API 연동 전 로컬 정리)
+     */
     override suspend fun deleteAccount() {
-        delay(300)
+        pushTokenManager.unregisterCurrentDevice()
+        tokenDataStore.clearTokens()
     }
 }
