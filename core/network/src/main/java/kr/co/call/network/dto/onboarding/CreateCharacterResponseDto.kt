5014d2750075149678c9ef6f592b8a1275cc8a00
ljw42b <80021912+JiwonLee42@users.noreply.github.com>
2026-08-09 14:51:42 +0900

    fix: 캐릭터 생성 응답 DTO characterId 매핑 추가

--- core/network/src/main/java/kr/co/call/network/dto/onboarding/CreateCharacterResponseDto.kt
+++ core/network/src/main/java/kr/co/call/network/dto/onboarding/CreateCharacterResponseDto.kt
@@ -1,6 +1,12 @@
 package kr.co.call.network.dto.onboarding
 
+import com.google.gson.annotations.SerializedName
+
 data class CreateCharacterResponseDto(
+    @SerializedName("characterId")
     val id: Long,
+    @SerializedName("name")
     val name: String?,
+    @SerializedName("imageUrl")
+    val characterImageUrl: String?,
 )
